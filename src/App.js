b16e98adcaff24494f38310e0078eb71dd82b6db
Kalana_bro <124587281+kalanajanana@users.noreply.github.com>
2024-06-23 14:13:07 +0530

    Create a react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Homepage</h1>
+      <div>
+        <h2>fuk</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
