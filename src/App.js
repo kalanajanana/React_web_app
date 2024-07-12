d77b3e3eeb0a06b9e7d619c730dd896abfc04f33
Kalana_bro <124587281+kalanajanana@users.noreply.github.com>
2024-07-12 11:43:39 +0530

    Add new js files and images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Homepage</h1>
-    </div>
+    <>
+    <Nav/>
+    </>
   );
 }
+
 
 export default App;
